f495acbad3f851e704fb0a9efa0c8de9d2492494
rjmatthews62 <robbie@mithril.com.au>
2019-06-23 16:34:01 +1000

    Turned povconvert into a class.

--- povconvert.py
+++ povconvert.py
@@ -9,6 +9,7 @@ class PovConvert:
     PIXELS=11
     XRESOLUTION=160
     GAP=16
+    OFFSET=0
 
 
     def cvtimage(self, filename:str, destination:str):
@@ -17,26 +18,32 @@ class PovConvert:
         result.save(destination,"PNG")
         
     def transformimage(self,graphic:Image):
-        img = Image.new('RGB', (self.XRESOLUTION-self.GAP, self.PIXELS))
+        img = Image.new("L", (self.XRESOLUTION-self.GAP, self.PIXELS))
         w,h = graphic.size
         cx=w//2
         cy=h//2
         radius=min(cx,cy)
         ratio=(self.OUTER_DIAMETER-self.INNER_DIAMETER)/self.PIXELS
-        imgdata=graphic.load()
+        rpixels=max(1,math.floor(ratio))
+        imgdata=graphic.convert("L").load(); # Convert to levels.
         destmap=img.load()
         for x in range(self.XRESOLUTION-self.GAP):
-                angle=math.radians(x*360/self.XRESOLUTION)
+                angle=math.radians(x*360/self.XRESOLUTION+self.OFFSET)
                 mx=math.cos(angle)
                 my=math.sin(angle)
                 for y in range(self.PIXELS):
                         ny=(y*ratio)+self.INNER_DIAMETER
                         mr=ny * radius/self.OUTER_DIAMETER
+                        pixel=0
+                        cnt=0
                         px=round(mr*mx)+cx
                         py=round(mr*my)+cy
                         pixel=imgdata[px,py]
                         destmap[x,(self.PIXELS-y)-1]=pixel
         return img
+
+    def getbwimage(self,img:Image) -> Image:
+        return img.convert(mode="1")
 
     def buildimage(self,filename:str,colour:Colour=Colour.white,inverted:bool=False):
         img = Image.new('RGB', (Message.MAX_COLUMNS, Column.PIXELS))
@@ -71,12 +78,16 @@ class PovConvert:
 
 
 if __name__ == "__main__":
-    src="testcircle1.png"
-    dst="testcvt1.png"
+    src="testcircle3.png"
+    dst="testcvt3.png"
     pc=PovConvert()
-    pc.cvtimage(src,dst)
+    pc.OFFSET=235
+    img=Image.open(src)
+    img=pc.transformimage(img)
+    img.save(dst);
+    #pc.cvtimage(src,dst)
     print("Saved as ",dst)
-    m4 = pc.makemessage(dst,Colour.cyan,OpenTransition.Clockwise,MessageStyle.Clockwise,CloseTransition.ToMiddle,False)
+    m4 = pc.makemessage(dst,Colour.cyan,OpenTransition.All,MessageStyle.Remain,CloseTransition.UpDown,False)
 
     p = Program((m4,))
     d = Device()
